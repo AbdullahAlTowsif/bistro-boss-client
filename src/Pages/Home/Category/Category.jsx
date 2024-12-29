62918d3f1b7a869edfcf1a113e15ed875a37faae
AbdullahAlTowsif <abdullah.al.towsif2002@gmail.com>
2024-12-29 16:57:07 +0600

    popular menu done

--- src/Pages/Home/Category/Category.jsx
+++ src/Pages/Home/Category/Category.jsx
@@ -4,7 +4,6 @@ import { Pagination } from 'swiper/modules';
 import 'swiper/css';
 import 'swiper/css/pagination';
 
-
 import slide1 from "../../../assets/home/slide1.jpg"
 import slide2 from "../../../assets/home/slide2.jpg"
 import slide3 from "../../../assets/home/slide3.jpg"
