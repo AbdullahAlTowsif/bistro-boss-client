67ac8d1ea743e2ae313858e2def89b1b47a3070e
AbdullahAlTowsif <abdullah.al.towsif2002@gmail.com>
2024-12-29 19:05:49 +0600

    button added to popular menu

--- src/Pages/Home/PopularMenu/PopularMenu.jsx
+++ src/Pages/Home/PopularMenu/PopularMenu.jsx
@@ -20,6 +20,9 @@ const PopularMenu = () => {
                     menu.map(item => <MenuItem key={item._id} item={item}></MenuItem>)
                 }
             </div>
+            <div className="text-center mt-5">
+            <button className="btn btn-outline border-0 border-b-4 mt-4">View Full Menu</button>
+            </div>
         </section>
     );
 };
