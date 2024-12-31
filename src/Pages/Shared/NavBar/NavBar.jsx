998b5beba3864b70e233a854c68886fda39777ff
AbdullahAlTowsif <abdullah.al.towsif2002@gmail.com>
2025-01-01 03:23:05 +0600

    private route added in different way

--- src/Pages/Shared/NavBar/NavBar.jsx
+++ src/Pages/Shared/NavBar/NavBar.jsx
@@ -1,11 +1,33 @@
+import { useContext } from "react";
 import { Link } from "react-router-dom";
+import { AuthContext } from "../../../providers/AuthProvider";
 
 const NavBar = () => {
+    const { user, logOut } = useContext(AuthContext);
+
+    const handleLogOut = () => {
+        logOut()
+        .then(() => {})
+        .catch(error => console.log(error))
+    }
+
     const navOptions = <>
         <li><Link to="/">Home</Link></li>
         <li><Link to="/menu">Our Menu</Link></li>
         <li><Link to="/order/salad">Order Food</Link></li>
+        <li><Link to="/secret">Secret</Link></li>
+
+        {
+            user ?
+                <>
+                    <button onClick={handleLogOut} className="btn btn-ghost">Logout</button>
+                </> :
+                <>
+                    <li><Link to="/login">Login</Link></li>
+                </>
+        }
     </>
+    
     return (
         <div>
             <div className="navbar fixed z-10 bg-opacity-30 max-w-screen-xl bg-black text-white">
