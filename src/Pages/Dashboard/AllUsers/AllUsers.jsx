01e08b32f22c22a9d0a6f8960a10f007237d39e0
AbdullahAlTowsif <abdullah.al.towsif2002@gmail.com>
2025-01-05 05:18:34 +0600

    jwt ongoing...

--- src/Pages/Dashboard/AllUsers/AllUsers.jsx
+++ src/Pages/Dashboard/AllUsers/AllUsers.jsx
@@ -1,11 +1,12 @@
 import { useQuery } from "@tanstack/react-query";
 import useAxiosSecure from "../../../hooks/useAxiosSecure";
 import { FaTrash, FaUsers } from "react-icons/fa";
+import Swal from "sweetalert2";
 
 const AllUsers = () => {
     const axiosSecure = useAxiosSecure();
 
-    const { data: users = [] } = useQuery({
+    const { data: users = [], refetch } = useQuery({
         queryKey: ['users'],
         queryFn: async () => {
             const res = await axiosSecure.get('/users');
@@ -13,8 +14,48 @@ const AllUsers = () => {
         }
     })
 
-    const handleDeleteUser = () => {
+    const handleMakeAdmin = (user) => {
+        axiosSecure.patch(`/users/admin/${user._id}`)
+            .then(res => {
+                console.log(res.data)
+                if (res.data.modifiedCount > 0) {
+                    refetch();
+                    Swal.fire({
+                        position: "top-end",
+                        icon: "success",
+                        title: `${user.name} is now Admin`,
+                        showConfirmButton: false,
+                        timer: 1500
+                    });
+                }
+            })
+    }
 
+    const handleDeleteUser = (user) => {
+        Swal.fire({
+            title: "Are you sure?",
+            text: "You won't be able to revert this!",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Yes, delete it!"
+        }).then((result) => {
+            if (result.isConfirmed) {
+                axiosSecure.delete(`/users/${user._id}`)
+                    .then(res => {
+                        // console.log(res);
+                        if (res.data.deletedCount > 0) {
+                            refetch();
+                            Swal.fire({
+                                title: "Deleted!",
+                                text: "User has been Removed.",
+                                icon: "success"
+                            });
+                        }
+                    })
+            }
+        });
     }
     return (
         <div>
@@ -41,12 +82,15 @@ const AllUsers = () => {
                                 <td>{user.name}</td>
                                 <td>{user.email}</td>
                                 <td>
-                                    <button onClick={() => handleDeleteUser(item._id)} className="btn btn-lg bg-orange-500">
-                                        <FaUsers className="text-white text-xl"></FaUsers>
-                                    </button>
+                                    {
+                                        user.role === 'admin' ? 'Admin' :
+                                        <button onClick={() => handleMakeAdmin(user)} className="btn btn-lg bg-orange-500">
+                                            <FaUsers className="text-white text-xl"></FaUsers>
+                                        </button>
+                                    }
                                 </td>
                                 <td>
-                                    <button onClick={() => handleDeleteUser(item._id)} className="btn btn-lg bg-red-500">
+                                    <button onClick={() => handleDeleteUser(user)} className="btn btn-lg bg-red-500">
                                         <FaTrash className="text-white text-xl"></FaTrash>
                                     </button>
                                 </td>
--- src/providers/AuthProvider.jsx
+++ src/providers/AuthProvider.jsx
@@ -42,6 +42,11 @@ const AuthProvider = ({children}) => {
     useEffect(()=> {
         const unsubscribe =  onAuthStateChanged(auth, currentUser => {
             setUser(currentUser);
+            if(currentUser){
+                // get token and store client
+            }else{
+                // Remove token if token stored in the client side
+            }
             console.log('current user --->', currentUser);
             setLoading(false);
         })
